0974561c0965dd6ac2cbb857bbaadb204318c838
jpptm <prncmontano@gmail.com>
2022-10-08 16:35:16 +1100

    util scripts

---
+++ SENG2250/A3/cbc.py
@@ -0,0 +1,101 @@
+import hashlib
+from Crypto.Cipher import AES
+import secrets
+
+def cbc_encrypt(plaintext, key, nonce):
+   # Initialise encryption object and turn nonce to 16 bytes. nonce will initially be secrets.token_urlsafe(16) (str) and will be converted to bytes
+    ciphertext = []
+    aes = AES.new(key, AES.MODE_ECB)
+    nonce = (int('0x'+hashlib.sha256(str(nonce).encode()).hexdigest(),16) % 2**128).to_bytes(16,'big')
+
+    # Transform plaintext to bytes
+    plaintext = str(plaintext).encode()
+
+    # Split the plaintext into blocks of 16 bytes
+    plaintext_blocks = [plaintext[i:i+16] for i in range(0, len(plaintext), 16)]
+    
+    for block in plaintext_blocks:
+        # XOR the block with the nonce
+        block = bytewiseXOR(block, nonce)
+        # Encrypt the block
+        block = aes.encrypt(block)
+        # Append the ciphertext to the list after converting it to hex
+        ciphertext.append(block.hex())
+        # Set the nonce to the ciphertext
+        nonce = block
+
+    return "".join([c for c in ciphertext])
+
+def cbc_decrypt(ciphertext, key, nonce):
+    # Initialise decryption object and turn nonce to 16 bytes. nonce will initially be secrets.token_urlsafe(16) (str) and will be converted to bytes
+    plaintext = []
+    aes = AES.new(key, AES.MODE_ECB)
+    nonce = (int('0x'+hashlib.sha256(str(nonce).encode()).hexdigest(),16) % 2**128).to_bytes(16,'big')
+
+    # Split ciphertext to blocks again, this time by increments of 32 so we get our four blocks back
+    ciphertext_blocks = [ciphertext[i:i+32] for i in range(0, len(ciphertext), 32)]
+
+    # Decrypt each block
+    for block in ciphertext_blocks:
+        # Each ciphertext block is in string form, without the 0x. We need to convert it back to bytes
+        block = int("0x" + block, 16).to_bytes(16, 'big')
+        # Decrypt the current block, xor it with the nonce and append it to the plaintext list
+        plaintext_block = aes.decrypt(block)
+        plaintext_block = bytewiseXOR(plaintext_block, nonce)
+        plaintext.append(plaintext_block)
+        # Set the next nonce as the current block
+        nonce = block
+
+    return "".join([p.decode() for p in plaintext])
+
+def hashed_mac(message, key):
+    # Initialise padding constants
+    opad = int("0x" + "5c" * len(key), 16).to_bytes(len(key), 'big')
+    ipad = int("0x" + "36" * len(key), 16).to_bytes(len(key), 'big')
+   
+    # Follow the formula
+    key_xor_opad = bytewiseXOR(key, opad)
+    key_xor_ipad = bytewiseXOR(key, ipad)
+
+    # Concatenate message and key_xor_ipad then hash the result
+    key_xor_ipad_message = key_xor_ipad + message.encode()
+    hashed_key_xor_ipad_message = hashlib.sha256(key_xor_ipad_message).digest()
+    
+    # Concatenate hashed_key_xor_ipad_message and key_xor_opad then hash the result
+    concat = key_xor_opad + hashed_key_xor_ipad_message
+
+    # Return the hexdigest of the hash
+    return hashlib.sha256(concat).hexdigest()
+
+def bytewiseXOR(a, b):
+    # Zip the two byte arrays together, tuple unpack each pair, XOR them and convert back to bytes
+    return bytes([x ^ y for x, y in zip(a, b)])
+
+def CBCTest():
+    nonce = secrets.token_urlsafe(16)
+    
+    print(nonce)
+    print(type(nonce))
+
+    key = 101153146228630395303830694709631737433950628137676538287650741735588006718384388996669347164145838162561867995798027180391347274917152763965286860663592510856353735958012961113658933327330290995775717880049638211397218575432070933547824779245933055388430284894534999613188410321834753695155190531750947931967
+    k_hash = hashlib.sha256(key.to_bytes(1024,'big'))
+    k_ = format(int.from_bytes(k_hash.digest(), 'big'),'#0258b')[2:258]
+    k_digest = k_hash.digest()
+
+    message = 'this message is somehow crazily enough exactly sixty four bytes.'
+    print(message)
+
+    encryptedMessage = cbc_encrypt(message,k_digest,nonce)
+    print(encryptedMessage)
+    
+    decryptedMessage = cbc_decrypt(encryptedMessage,k_digest,nonce)
+    print(decryptedMessage)
+
+def test():
+    key = 101153146228630395303830694709631737433950628137676538287650741735588006718384388996669347164145838162561867995798027180391347274917152763965286860663592510856353735958012961113658933327330290995775717880049638211397218575432070933547824779245933055388430284894534999613188410321834753695155190531750947931967
+    k_hash = hashlib.sha256(key.to_bytes(1024,'big'))
+    k_digest = k_hash.digest()
+    message = 'ahis message is somehow crazily enough exactly sixty four bytes.'
+
+    print('message:',message)
+    print('hmac:\t',hashed_mac(message,k_digest))
---
+++ SENG2250/A3/rsa.py
@@ -0,0 +1,35 @@
+from util import *
+
+# This script will handle message encryption and decryption using RSA
+class RSA:
+    def __init__(self):
+        
+        # Generate rsa keys by using functions from the util module
+        # Flag p and q as private members. They must not be touched from outside the class
+        p = generate_prime(1024)
+        q = generate_prime(1024)
+
+        # RSA pub key
+        self.e = 65537
+
+        # n = pq
+        self.n = p * q
+
+        # Find phi_n
+        phi_p = p - 1
+        phi_q = q - 1
+        phi_n = phi_p * phi_q
+
+        # Find decryption key
+        d = find_mod_inv(self.e, phi_n)
+        self.public_key = (self.e, self.n)
+        # Note that a real implementation should probably be written in a language that puts more emphasis on encapsulation
+        self.__private_key = (p, q, d)
+
+    # Functions for encrypting and decrypting a msg
+    def encrypt(self, msg):
+        return fast_mod(msg, self.e, self.n)
+
+    def decrypt(self, msg):
+        return fast_mod(msg, self.__private_key[2], self.n)
+        
